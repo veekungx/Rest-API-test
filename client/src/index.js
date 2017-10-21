66ffc3e4ad4f281034c9a0ab50e9da81960ba14b
Ti <veekungx@gmail.com>
2017-10-21 16:45:43 +0700

    Setup redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './config-store';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
