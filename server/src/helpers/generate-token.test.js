aa6f0395c7831aea3f5c98278fcc246d268d06aa
Ti <veekungx@gmail.com>
2017-10-22 00:13:01 +0700

    refactor generateToken

--- server/src/helpers/generate-token.test.js
+++ server/src/helpers/generate-token.test.js
@@ -6,10 +6,13 @@ const generateToken = require('./generate-token');
 describe('generateToken', () => {
   it('should generate token', () => {
     const userId = '1234';
-    const token = generateToken(userId);
+    const { token } = generateToken(userId);
     const decoded = jwt.decode(token, secret);
-
     expect(decoded.userId).to.equal(userId);
     expect(decoded.access).to.equal('auth');
+  });
+
+  it('should throw error when no userId given', () => {
+    expect(() => generateToken()).to.throw('no user id');
   });
 });
