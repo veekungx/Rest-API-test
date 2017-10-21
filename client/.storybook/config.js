1f43f49f831ad7de78b6260d478a6f9af7969640
Ti <veekungx@gmail.com>
2017-10-21 20:31:57 +0700

    add variables

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,16 @@
+import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import configStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+const mockStore = configStore();
+const store = mockStore();
+
+addDecorator(story =>
+  <Provider store={store}>
+    {story()}
+  </Provider>
+);
+
 const req = require.context("../src", true, /\.story\.js$/);
 const loadStories = () => {
   req.keys().forEach((filename) => req(filename))
