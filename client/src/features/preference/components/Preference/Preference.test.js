d60e3b758d053b1fabffd2131be9871b3bcc4392
Ti <veekungx@gmail.com>
2017-10-23 12:09:49 +0700

    improve submitting on save button

--- client/src/features/preference/components/Preference/Preference.test.js
+++ client/src/features/preference/components/Preference/Preference.test.js
@@ -6,5 +6,17 @@ describe('Preference', () => {
   it('should render');
 
   describe('Components', () => {
+    it('should not show "SAVE PREFERENCE" on save button by default', () => {
+      const wrapper = shallow(<Preference />);
+      expect(wrapper.find('.Preference__saveButton').text()).toContain('SAVE PREFERENCE');
+    });
   });
+
+  describe('Props', () => {
+    it('should render "SAVING" on save button when submitting is given', () => {
+      const wrapper = shallow(<Preference submitting />);
+      expect(wrapper.find('.Preference__saveButton').text()).toContain('SAVING');
+    });
+  });
+  describe('Events', () => { });
 });
